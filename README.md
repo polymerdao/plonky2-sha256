835051c555574fb9b41f256c696660c3d71a76c2
Sai Deng <dengsai@hotmail.com>
2022-09-07 16:22:50 -0700

    Copy from plonky2-sha512

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # plonky2-sha256
+
+This repository contains [SNARK](https://en.wikipedia.org/wiki/Non-interactive_zero-knowledge_proof) circuits of a
+cryptographic hash function [SHA-256](https://en.wikipedia.org/wiki/SHA-2) implemented
+with [Plonky2](https://github.com/mir-protocol/plonky2).
+
+Run benchmarks
+
+```console
+RUSTFLAGS=-Ctarget-cpu=native cargo run --release --package plonky2_sha256 --bin plonky2_sha256
+```
+
+Benchmark on a Macbook Pro (M1), preimage message size = 128 (block count = 2)
+
+```console
+
+```
---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use anyhow::Result;
+use log::{Level, LevelFilter};
+use plonky2::iop::witness::{PartialWitness, Witness};
+use plonky2::plonk::circuit_builder::CircuitBuilder;
+use plonky2::plonk::circuit_data::CircuitConfig;
+use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
+use plonky2::util::timing::TimingTree;
+use plonky2_sha256::circuit::{array_to_bits, make_circuits};
+use sha2::{Digest, Sha256};
+
+pub fn prove_sha256(msg: &[u8]) -> Result<()> {
+    let mut hasher = Sha256::new();
+    hasher.update(msg);
+    let hash = hasher.finalize();
+    // println!("Hash: {:#04X}", hash);
+
+    let msg_bits = array_to_bits(msg);
+    let len = msg.len() * 8;
+    const D: usize = 2;
+    type C = PoseidonGoldilocksConfig;
+    type F = <C as GenericConfig<D>>::F;
+    let mut builder = CircuitBuilder::<F, D>::new(CircuitConfig::standard_recursion_config());
+    let targets = make_circuits(&mut builder, len as u128);
+    let mut pw = PartialWitness::new();
+
+    for i in 0..len {
+        pw.set_bool_target(targets.message[i], msg_bits[i]);
+    }
+
+    let expected_res = array_to_bits(hash.as_slice());
+    for i in 0..expected_res.len() {
+        if expected_res[i] {
+            builder.assert_one(targets.digest[i].target);
+        } else {
+            builder.assert_zero(targets.digest[i].target);
+        }
+    }
+
+    println!(
+        "Constructing inner proof with {} gates",
+        builder.num_gates()
+    );
+    let data = builder.build::<C>();
+    let timing = TimingTree::new("prove", Level::Debug);
+    let proof = data.prove(pw).unwrap();
+    timing.print();
+
+    let timing = TimingTree::new("verify", Level::Debug);
+    let res = data.verify(proof);
+    timing.print();
+
+    res
+}
+
+fn main() -> Result<()> {
+    // Initialize logging
+    let mut builder = env_logger::Builder::from_default_env();
+    builder.format_timestamp(None);
+    builder.filter_level(LevelFilter::Debug);
+    builder.try_init()?;
+
+    const MSG_SIZE: usize = 128;
+    let mut msg = vec![0; MSG_SIZE as usize];
+    for i in 0..MSG_SIZE - 1 {
+        msg[i] = i as u8;
+    }
+    prove_sha256(&msg)
+}
